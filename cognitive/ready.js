d6d775b9f7dbeb0a6571137519c1fc7ae4e959f9
Sadickachuli <m.achuli@alustudent.com>
2023-12-04 17:03:45 +0200

    new

---
+++ cognitive/ready.js
@@ -0,0 +1,9 @@
+function redirectToNextPage() {
+    // Redirect to the next page (replace 'nextPage.html' with the actual URL)
+    window.location.href = 'questions.html';
+}
+
+function redirectToHomePage() {
+    // Redirect to the home page (replace 'home.html' with the actual URL)
+    window.location.href = 'home.html';
+}
