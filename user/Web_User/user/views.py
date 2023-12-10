aa1f6d03654d99142867b521b2ce9ce4f0622097
Emmanuel-Begati <e.yakubu@alustudent.com>
2023-12-10 23:08:54 +0200

    done

--- user/Web_User/user/views.py
+++ user/Web_User/user/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from forms import MyModelForm
+# from forms import MyModelForm
 # Create your views here.
 def sign_up(request):
     if request.method == 'POST':
